2ab7ea57e4b4ca1883ae42dc533902680da77a41
herre <donatus.herre@slub-dresden.de>
2021-12-09 19:00:01 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="serialj",
-    version="0.2.5",
+    version="0.2.6",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Parse JSON serialized MARC or PICA data.",
