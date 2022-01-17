2911047c3f7698d4f42fc7168ff0e08512b8f5a2
herre <donatus.herre@slub-dresden.de>
2022-01-17 19:31:05 +0100

    Update package metadata

--- serialj/__init__.py
+++ serialj/__init__.py
@@ -1,6 +1,9 @@
+"""
+Parse JSON serialized MARC and PICA data
+"""
+
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
 __version__ = "0.2.7"
-__license__ = "GPLv3"
 
 from . import utils
 from .marcjson import MarcJson
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     version="0.2.7",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
-    description="Parse JSON serialized MARC or PICA data.",
+    description="Parse JSON serialized MARC and PICA data",
     license=open("LICENSE").read(),
     url="https://github.com/herreio/serialj",
     packages=["serialj"],
