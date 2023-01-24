e2ed1cd630616090883ff7224956a3e334c19e1e
Donatus Herre <donatus.herre@slub-dresden.de>
2023-01-24 19:47:00 +0100

    Bump version number

--- serialj/__init__.py
+++ serialj/__init__.py
@@ -3,7 +3,7 @@ Parse JSON serialized MARC and PICA data
 """
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "0.2.14"
+__version__ = "0.2.15"
 
 from . import utils
 from .marcjson import MarcJson
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="serialj",
-    version="0.2.14",
+    version="0.2.15",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Parse JSON serialized MARC and PICA data",
